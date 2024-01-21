c3d8c55505b039ed9c150a63f05910321b5bbb93
Sharmaakri10 <120705446+Sharmaakri10@users.noreply.github.com>
2024-01-21 22:30:03 +0530

    done code in js

--- sum.js
+++ sum.js
@@ -1,4 +1,24 @@
-function sum(a, b) {
-    return a + b
+// // function sum(a, b) {
+// //     return a + b
+// // }
+
+
+// // module.exports = sum
+
+// function checkvalidinput(inp) {
+//     if (typeof inp !== 'number') {
+//         throw new Error('Invalid Input')
+//     } else {
+//         throw new Error('valid Input')
+
+
+//     }
+// }
+// module.exports = checkvalidinput
+function FETCHdATA(callback) {
+    setTimeout(() => {
+        callback('peanut butter')
+    }, 1000);
 }
-module.exports = sum
+
+module.exports = FETCHdATA
--- sum.test.js
+++ sum.test.js
@@ -1,14 +1,43 @@
-const sum = require('./sum')
-test('adds 2 +3  equal to 5description', () => {
-    expect(sum(2, 3)).toBe(5) //toBe IS MATCHER IS EXPECT THE VALUE IS MAENTIONS ONLY FOR PRIMITIVE VALUES
-})
-test('two plus two is four', () => {
-    expect(2 + 4).toBe(6)
-})
-test('OBJECT assignment', () => {
-    const data = { one: 1 };
-    data['two'] = 2,
-        data['three'] = 2
+// const sum = require('./sum')
+const checkvalidinput = require('./sum')
 
-    expect(data).toEqual({ one: 1, two: 2, three: 3 })
+const fetch = require('./sum')
+    // test('adds 2 +3  equal to 5description', () => {
+    //     expect(sum(2, 3)).toBe(5) //toBe IS MATCHER IS EXPECT THE VALUE IS MAENTIONS ONLY FOR PRIMITIVE VALUES
+    // })
+    // test('two plus two is four', () => {
+    //     expect(2 + 4).toBe(6)
+    // })
+    // test('OBJECT assignment', () => {
+    //     const data = { one: 1 };
+    //     data['two'] = 2,
+    //         data['three'] = 2
+
+//     expect(data).toEqual({ one: 1, two: 2, three: 3 })
+// })
+// test('n is falsy vale', () => {
+//     const n = ''
+//     expect(n).toBeFalsy()
+// })
+
+// test('n is thruthy vale', () => {
+//     const n = 1
+//     expect(n).toBeTruthy()
+// })
+
+// test('throws  VALID INPUT', () => {
+//     expect(() => {
+//         checkvalidinput(9);
+//     }).toThrow();
+// })
+test('the data is peanut  butter', done => {
+    function callback(data) {
+        try {
+            expect(data).toBe('peanut butter');
+            done();
+        } catch (er) {
+            done(er)
+        }
+    }
+    fetch(callback)
 })
